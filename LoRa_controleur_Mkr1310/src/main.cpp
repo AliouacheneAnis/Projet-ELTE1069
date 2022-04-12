d121258f0ac7a3ae37b5cc431f0c9e135d84f114
AliouacheneAnis <eaalioua01@monccnb.ca>
2022-04-12 01:12:12 -0300

    Version Final

--- LoRa_controleur_Mkr1310/src/main.cpp
+++ LoRa_controleur_Mkr1310/src/main.cpp
@@ -1,50 +1,60 @@
+// Ajout des libraires necessaire pour le projet 
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>                  
 #include <LoRa.h>
 
+// Declaration Variable string qui va contenir la chaine de caractere recu par LoraWan 
 String payload ;
-void receiveEvent(int howMany)  ;
+
 
 void setup() {
+
 	Serial.begin(9600);
-
-	while (!Serial);
-
 	Serial.println("LoRa Receiver");
-
+    
+	// communication Lorawan
 	if (!LoRa.begin(915E6)) {
 		Serial.println("Starting LoRa failed!");
 		while (1);
 	}
+
+	// Debut communication I2C 
   	Wire.begin();                       
 
 }
 
+
 void loop() {
+
 	// try to parse packet
 	int packetSize = LoRa.parsePacket();
-	if (packetSize) {
-		 payload = "";
-		// received a packet
-		Serial.print("Received packet : ");
 
-		// read packet
+	if (packetSize) {
+
+		 payload = ""; // initialisation
+
+		 Serial.print("Received packet : ");
+
+		// Lecture de packet Lorawan recu tantqu'il est disponible caractere par caractere et construction du string 
 		while (LoRa.available()) {
 			char c = LoRa.read(); 
 			payload  += c;      
 		}
-		Serial.println(payload);
-		// print RSSI of packet
-		Serial.print("\t with RSSI ");
-		Serial.println(LoRa.packetRssi());
+		Serial.println(payload);  // Affichage dans le moniteur serie la chaine de caractere recu 
 
+		Serial.print("\t with RSSI ");
+		Serial.println(LoRa.packetRssi()); // affichage signal LoraWan 
+
+        // Debut communication I2C sur l'adresse 4 afin d'envoyer la chaine de caractere vers le peripherique mkr1010 : envoi caractere par caracter (Tableau de caractere)
 		Wire.beginTransmission(4);
+
 		for (unsigned int i = 0; i < payload.length(); i++)
 		{
-			Wire.write(payload[i]);
+			Wire.write(payload[i]); // Envoi par I2C le caractere dans la case i 
 		}
-		Wire.endTransmission();
+
+		Wire.endTransmission(); // fin communication I2C 
 		Serial.println("Envoie via I2C réussie");
 	}
 }
--- Peripherique_UC_Mkr1010/src/main.cpp
+++ Peripherique_UC_Mkr1010/src/main.cpp
@@ -3,36 +3,37 @@
 #include "MQTTConnector.h"
 #include <Wire.h>
 
+// Fonction 
 void receiveEvent(int howMany)  ;
+
+// Setup 
 void setup()
 {
 	Serial.begin(9600);
-	while (!Serial);
-	wifiConnect();
-	MQTTConnect();
+
+	wifiConnect();  // Conncecter au wifi 
+	MQTTConnect();  // connecter au broker mqtt 
 
 	Serial.println("I2C Receiver");
-	Wire.begin(4);
-	Wire.onReceive(receiveEvent);
+	Wire.begin(4);  // debut communication I2C 
+	Wire.onReceive(receiveEvent);  // apeler la fonction a chaque fois on recois une donnees  par I2C 
 }
 
 
-void loop()
-{
-	
-}
+void loop(){}
 
-
+// fonction appller a chaque fois y a une donnees envoyer par I2C du la part du controleur 
 void receiveEvent(int howMany)      
 {  
  	Payload = "";
-	while(Wire.available())       
+
+	while(Wire.available())    // Tantque y a des donnees a recevoir via I2C 
 	{
-		char c = Wire.read(); 
-		Payload  += c;       
+		char c = Wire.read();  // lire le caractere recu 
+		Payload  += c;     // contruction de la chaine de caractere MQTT   
 	}
 	Serial.print("Chaine recue --> ");
-	Serial.println(Payload);        
-	sendPayload();     
+	Serial.println(Payload);        // affichage de la chaine de caractere MQTT 
+	sendPayload();     // Envoi de la chaine de caractere MQTT sur ThingsBoard 
 	
 }
